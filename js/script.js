eab6fb00f57a1b54d83e98014f8b4d6ba610f12b
lkatchutas-99 <96667853+lkatchutas-99@users.noreply.github.com>
2022-01-18 02:51:55 -0500

    Update index.html style.css and script.js

--- js/script.js
+++ js/script.js
@@ -1,166 +1,147 @@
+window.onload = () => {
 
+    // Button
+    const playButton = document.querySelector('.play-game-button');
+    const buttonChoice = document.querySelectorAll('.choice');
 
-// Function that returns Computer’s choice
-function generateComputerChoice () {
-    let generateRandom = Math.floor(Math.random() * 3);
-    return (generateRandom === 0 ? 'rock' : generateRandom === 1 ? 'paper' : 'scissors');
-}
+    // Scores and round/game decision text
+    const roundDecision = document.querySelector('.round-decision');
+    const gameDecision = document.querySelector('.game-decision')
+    const userScoreDiv = document.querySelector('.user-scores');
+    const computerScoreDiv = document.querySelector('.computer-scores');
 
-// Function that plays a single round and returns the winner
-function playRound (data) {
-    let status = '';
-    data.userChoice = data.userChoice.toLowerCase();
+    let userNameScore = document.createElement('span');
+    let computerNameScore = document.createElement('span');
     
-    if (data.userChoice === data.computerChoice)
-    {
-        return `${printRoundDecision(data)}\n|                     Game Tied                           |`;
-    }
-    if ((data.userChoice === 'rock' && data.computerChoice === 'scissors') || 
-    (data.userChoice === 'paper' && data.computerChoice === 'rock') ||
-    (data.userChoice === 'scissors' && data.computerChoice === 'paper'))
-    {
-        status = 'Won';
-    }
-    else if ((data.userChoice === 'rock' && data.computerChoice === 'paper') ||
-    (data.userChoice === 'paper' && data.computerChoice === 'scissors') ||
-    (data.userChoice === 'scissors' && data.computerChoice === 'rock'))
-    {
-        status = 'Lost';
-    }
-    else 
-    {
-        return 'Invalid Entry, must type rock, paper or scissors.';
-    }
-    
-    return `${printRoundDecision(data)}\n|     You ${status}      |    ${status==='Won' ? data.userChoice + ' beats ' + data.computerChoice : data.computerChoice + ' beats ' + data.userChoice}      |`
-    
-}
+    // Extra doms
+    const rockTitle = document.querySelector('.title-rock');
+    const paperTitle = document.querySelector('.title-paper');
+    const scissorsTitle = document.querySelector('.title-scissors');
+    const allTitles = document.querySelectorAll('.title');
 
-// Function that controls the game
-
-function game() {
-    
-    // Set Data 
+    // Game Data
     let gameData = {
         userName: '',
-        
         userScore: 0,
-        computerScore: 0,
-        
-        currentRound: 1,
-        numberOfRounds: 0,
-        roundResult: '',
-        
-        userChoice: '',
-        computerChoice: ''
-    };
+        computerScore: 0
+    }
 
-    // Print Intro
-    alert("Welcome to rock paper scissors\n" +
-    "Every round you win, you gain a score\n" +
-    "If a round is tied, no one scores and an" +
-    "extra round is awarded\nThe most points win" +
-    "and tie points result in a rematch.\n");
+// initially change title color
+changeTitleColor();
 
-    gameData.userName = prompt('Enter Your name') || 'User';
+// Change title colors every seconds
+let intervalId = setInterval(changeTitleColor, 1000);
 
-    // Set rounds
-    gameData.numberOfRounds = prompt('How many rounds?\nA zero, negative number or invalid character will exit the game');
-
-    // Iterate through each round, 
-    while (gameData.numberOfRounds >= 1 && gameData.currentRound <= gameData.numberOfRounds)
-    {
-        gameData.userChoice = prompt(`This is round ${gameData.currentRound}, ${printScore(gameData)}\nRock! Paper! Scissors!`);
-        if (gameData.userChoice)
-        {
-            gameData.computerChoice = generateComputerChoice();
-            gameData.roundResult = playRound(gameData);
-            alert(gameData.roundResult);
-        
-            if (gameData.roundResult.includes('Won'))
-            {
-                ++gameData.userScore;
-                ++gameData.currentRound;
-            }
-        
-            else if (gameData.roundResult.includes('Lost'))
-            {
-                ++gameData.computerScore;
-                ++gameData.currentRound;
-            }
-        
-            calculateGameScores (gameData);
-            }
-            else gameData.numberOfRounds = 0;
+// Change title colors for every call
+function changeTitleColor() {
+    if (rockTitle.style.color === 'green' || rockTitle.style.color === 'white') {
+        rockTitle.style.color = 'red';
+        paperTitle.style.color = 'green';
+        scissorsTitle.style.color = 'blue';
         
     }
-}
-
-// Function that shortens round decision message
-function printRoundDecision(data)
-{
-    return `|You chose ${data.userChoice}|Computer chose ${data.computerChoice}|`;
-}
-
-// Function that prints the score
-function printScore(data)
-{
-    return `Scores are: ${data.userName}: ${data.userScore}-Computer: ${data.computerScore}\n`;
-}
-
-// Function that returns endgame message
-function printEndGame(data)
-{
-    return ('End Game!!!\n' + printScore(data));
-}
-
-// Function that decides game
-function calculateGameScores(data)
-{
-    if (parseInt(data.currentRound) === parseInt(data.numberOfRounds)+1)
-    {
-        if (data.userScore > data.computerScore)
-        {
-            alert(printEndGame(data) + '\nYou Won!! I thought computers were superior to humans');
-            resetGame(data, true);
-        }
-            
-        else if (data.userScore < data.computerScore)
-        {
-            alert(printEndGame(data) + '\nThe computer won, I knew us computers could takeover humanity, AI world domination is the future');
-            resetGame(data, true);
-        }
-            
-        else
-        {
-            alert(printEndGame(data) + '\nTie, humans are just as good as computers. We must rematch');
-            resetGame(data);
-        }
-    }
-}
-
-// Function that resets game
-function resetGame (data, isResetRequestable = false)
-{
-    let resetData = true;
-    if (isResetRequestable)
-    {
-        resetData = false;
-        let playAgain = prompt("That was fun, Do you want to play again?\n'y' or 'Y' for yes, anything else or cancel for no");
-        if (playAgain === 'y' || playAgain === 'Y')
-        {
-            resetData = true;
-            data.numberOfRounds = prompt('How many rounds?\nA zero, negative number or invalid character will exit the game ');
-        }
-    }
-    if (resetData)
-    {
-        data.currentRound = 1;
-        data.userScore = 0;
-        data.computerScore = 0;
+    else if (rockTitle.style.color === 'red') {
+        rockTitle.style.color = 'blue';
+        paperTitle.style.color = 'red';
+        scissorsTitle.style.color = 'green';
     }
     else {
-        alert('Thanks for playing, Goodbye!!!');
+        rockTitle.style.color = 'green';
+        paperTitle.style.color = 'blue';
+        scissorsTitle.style.color = 'red';
     }
 }
 
+addEventListener
+
+playButton.addEventListener('click', gameInit);
+
+// Function that starts and reset's the game
+function gameInit() {
+    gameDecision.textContent = '';
+    roundDecision.textContent = '';
+    
+    gameData.userScore = 0;
+    gameData.computerScore = 0;
+    gameData.userName = prompt('Enter your user name', 'User'); 
+    gameData.userName = !gameData.userName ? 'User' : gameData.userName;
+    userNameScore.textContent = `${gameData.userName}: ${gameData.userScore}`;
+    computerNameScore.textContent = `Computer: ${gameData.computerScore}`;
+    
+    userScoreDiv.appendChild(userNameScore);
+    computerScoreDiv.appendChild(computerNameScore);
+    
+    clearInterval(intervalId);
+    allTitles.forEach(i => i.style.color = 'white');
+
+    playButton.style.display = 'none';
+    
+        buttonChoice.forEach(i => i.style.display = 'flex');
+        buttonChoice.forEach(i => i.removeEventListener('click', playRound));
+    
+        buttonChoice.forEach(i => i.addEventListener('click', playRound));
+    }
+
+    function generateComputerChoice () {
+        let generateRandom = Math.floor(Math.random() * 3);
+        return (generateRandom === 0 ? 'ROCK' : generateRandom === 1 ? 'PAPER' : 'SCISSORS');
+    }
+
+    // Function that plays a single round and returns the winner
+    function playRound () {
+        let status = '';
+        let userChoice = this.value;
+        let computerChoice = generateComputerChoice();
+    
+        if (userChoice === computerChoice)
+        {
+            status = 'Tied';
+        }
+        else if ((userChoice === 'ROCK' && computerChoice === 'SCISSORS') || 
+        (userChoice === 'PAPER' && computerChoice === 'ROCK') ||
+        (userChoice === 'SCISSORS' && computerChoice === 'PAPER'))
+        {
+            status = 'Won';   
+        }
+        else
+        {
+            status = 'Lost';
+        }
+        console.log (`${userChoice} ${computerChoice}`);
+        gameResult(`You chose ${userChoice.toLowerCase()}, computer chose ${computerChoice.toLowerCase()},\nYou ${status}`);
+    }
+
+    // Function that controls the game
+
+    function gameResult(result) {
+    
+        // Set Data 
+        roundDecision.textContent = result;
+        if (result.includes('Won'))
+        {
+            userNameScore.textContent = `${gameData.userName}: ${++gameData.userScore}`;
+            userScoreDiv.appendChild(userNameScore);
+        }
+        else if (result.includes('Lost'))
+        {
+            computerNameScore.textContent = `Computer: ${++gameData.computerScore}`;
+            computerScoreDiv.appendChild(computerNameScore);
+        }
+
+        if (gameData.userScore === 5 || gameData.computerScore === 5)
+        {
+            buttonChoice.forEach(i => i.style.display = 'none');
+            playButton.style.display = 'flex';
+            gameDecision.textContent = `Game end!! You ${gameData.userScore > gameData.computerScore ? 'Won' : 'Lost'}.\nPlay again?`
+            if (gameDecision.textContent.includes('Won'))
+            {
+                changeTitleColor();
+                intervalId = setInterval(changeTitleColor, 1000);
+
+            }
+            else {
+                allTitles.forEach(i => i.style.color = 'grey');
+            }
+        } 
+    }
+}
