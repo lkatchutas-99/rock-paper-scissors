f79232f08f1a0b35c96c85c677b4613fa013457a
lkatchutas-99 <96667853+lkatchutas-99@users.noreply.github.com>
2022-01-19 12:55:28 -0500

    Markup, style and script completed
    Cleaned up fine details and seperated script functions to ensure functions remain less than 30 lines.  This commit ensures that the main project is completed and that modifications can procede.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 window.onload = () => {
 
-    // Button
+    // Buttons
     const playButton = document.querySelector('.play-game-button');
     const buttonChoice = document.querySelectorAll('.choice');
 
-    // Scores and round/game decision text
+    // Score keeping
     const roundDecision = document.querySelector('.round-decision');
     const gameDecision = document.querySelector('.game-decision')
     const userScoreDiv = document.querySelector('.user-scores');
@@ -13,7 +13,7 @@ window.onload = () => {
     let userNameScore = document.createElement('span');
     let computerNameScore = document.createElement('span');
     
-    // Extra doms
+    // color changers
     const rockTitle = document.querySelector('.title-rock');
     const paperTitle = document.querySelector('.title-paper');
     const scissorsTitle = document.querySelector('.title-scissors');
@@ -27,120 +27,200 @@ window.onload = () => {
     }
 
 // initially change title color
-changeTitleColor();
+    changeTitleColor();
 
-// Change title colors every seconds
-let intervalId = setInterval(changeTitleColor, 1000);
+// add universal elements
+    playButton.addEventListener('click', gameInit);
+    buttonChoice.forEach(i => i.addEventListener('mouseover', changeTitleOnButtonHover));
+    buttonChoice.forEach(i => i.addEventListener('mouseout', setTitleWhite));
 
-// Change title colors for every call
-function changeTitleColor() {
-    if (rockTitle.style.color === 'green' || rockTitle.style.color === 'white') {
-        rockTitle.style.color = 'red';
-        paperTitle.style.color = 'green';
-        scissorsTitle.style.color = 'blue';
-        
-    }
-    else if (rockTitle.style.color === 'red') {
-        rockTitle.style.color = 'blue';
-        paperTitle.style.color = 'red';
-        scissorsTitle.style.color = 'green';
-    }
-    else {
-        rockTitle.style.color = 'green';
-        paperTitle.style.color = 'blue';
-        scissorsTitle.style.color = 'red';
-    }
-}
+    let buttonColorTimer;
+    let scoreColorTimer;
 
-addEventListener
+// Change title colors every 1 second
+    let titleColorTimer = setInterval(changeTitleColor, 1000);
 
-playButton.addEventListener('click', gameInit);
-
-// Function that starts and reset's the game
-function gameInit() {
-    gameDecision.textContent = '';
-    roundDecision.textContent = '';
-    
-    gameData.userScore = 0;
-    gameData.computerScore = 0;
-    gameData.userName = prompt('Enter your user name', 'User'); 
-    gameData.userName = !gameData.userName ? 'User' : gameData.userName;
-    userNameScore.textContent = `${gameData.userName}: ${gameData.userScore}`;
-    computerNameScore.textContent = `Computer: ${gameData.computerScore}`;
-    
-    userScoreDiv.appendChild(userNameScore);
-    computerScoreDiv.appendChild(computerNameScore);
-    
-    clearInterval(intervalId);
-    allTitles.forEach(i => i.style.color = 'white');
-
-    playButton.style.display = 'none';
-    
-        buttonChoice.forEach(i => i.style.display = 'flex');
-        buttonChoice.forEach(i => i.removeEventListener('click', playRound));
-    
-        buttonChoice.forEach(i => i.addEventListener('click', playRound));
+// Sets title colors to parameters
+    function setTitleColor (rockColor, paperColor, scissorsColor)
+    {
+        rockTitle.style.color = rockColor;
+        paperTitle.style.color = paperColor;
+        scissorsTitle.style.color = scissorsColor;
     }
 
+    // Change title colors whenever called
+    function changeTitleColor() {
+        if (rockTitle.style.color === 'green' || rockTitle.style.color === 'white') {
+            setTitleColor('red', 'green', 'blue');
+        }
+        else if (rockTitle.style.color === 'red') {
+            setTitleColor('blue', 'red', 'green');
+        }
+        else {
+            setTitleColor('green', 'blue', 'red');
+        }
+    }
+
+    // Change button everytime it is called
+    function changeButtonColor() {
+        if (playButton.style.backgroundColor === '#e9e9ed' || playButton.style.backgroundColor === 'green' || playButton.style.backgroundColor === '#d0d0d7')
+        {
+            playButton.style.backgroundColor = 'red';
+            playButton.style.color = 'green';
+        }
+        else {
+            playButton.style.backgroundColor = 'green';
+            playButton.style.color = 'red';
+        }
+    }
+
+    // change score color
+    function changeScoreColor () {
+        if (userScoreDiv.style.backgroundColor === 'lightgreen' || userScoreDiv.style.backgroundColor === 'white')
+        {
+            userScoreDiv.style.backgroundColor = 'red';
+            computerScoreDiv.style.backgroundColor = 'lightgreen';
+        }
+        else
+        {
+            userScoreDiv.style.backgroundColor = 'lightgreen';
+            computerScoreDiv.style.backgroundColor = 'red';
+        }
+    }
+
+    // set all titles to white
+    function setTitleWhite () {
+        allTitles.forEach(i => i.style.color = 'white');
+    }
+
+    // change all scores to white
+    function setScoresWhite (userOnly = false, computerOnly = false) {
+        if (userOnly || (!userOnly && !computerOnly)) {
+            userScoreDiv.style.backgroundColor = 'white';
+        }
+        if (computerOnly || (!userOnly && !computerOnly)) {
+            computerScoreDiv.style.backgroundColor = 'white';
+        }
+    }
+
+    // change title on choice button hover
+    function changeTitleOnButtonHover () {
+        if (this.value === 'ROCK') {
+            rockTitle.style.color = 'red';
+        }
+
+        else if (this.value === 'PAPER') {
+            paperTitle.style.color = 'green';
+        }
+
+        else {
+            scissorsTitle.style.color = 'blue';
+        }
+    }
+
+    // Start or reset game
+    function gameInit() {
+        gameDecision.textContent = '';
+        roundDecision.textContent = '';
+
+        gameData.userScore = 0;
+        gameData.computerScore = 0;
+
+        gameData.userName = prompt('Enter your user name', 'User'); 
+        gameData.userName = !gameData.userName ? 'User' : gameData.userName;
+
+        userNameScore.textContent = `${gameData.userName}: ${gameData.userScore}`;
+        computerNameScore.textContent = `Computer: ${gameData.computerScore}`;
+    
+        userScoreDiv.appendChild(userNameScore);
+        computerScoreDiv.appendChild(computerNameScore);
+    
+        clearInterval(buttonColorTimer);
+        clearInterval(titleColorTimer);
+        clearInterval(scoreColorTimer);
+
+        setScoresWhite();
+        setTitleWhite();
+
+        playButton.style.display = 'none';
+    
+        buttonChoice.forEach(i => {
+            i.style.display = 'flex';
+            i.addEventListener('click', playRound);
+            });
+        }
+
+        // Generate computer's choice
     function generateComputerChoice () {
         let generateRandom = Math.floor(Math.random() * 3);
         return (generateRandom === 0 ? 'ROCK' : generateRandom === 1 ? 'PAPER' : 'SCISSORS');
-    }
+        }
 
-    // Function that plays a single round and returns the winner
+        // Play single round
     function playRound () {
         let status = '';
         let userChoice = this.value;
         let computerChoice = generateComputerChoice();
     
-        if (userChoice === computerChoice)
-        {
+        if (userChoice === computerChoice) {
             status = 'Tied';
         }
         else if ((userChoice === 'ROCK' && computerChoice === 'SCISSORS') || 
         (userChoice === 'PAPER' && computerChoice === 'ROCK') ||
-        (userChoice === 'SCISSORS' && computerChoice === 'PAPER'))
+        (userChoice === 'SCISSORS' && computerChoice === 'PAPER')) 
         {
             status = 'Won';   
         }
-        else
-        {
+        else {
             status = 'Lost';
         }
-        console.log (`${userChoice} ${computerChoice}`);
-        gameResult(`You chose ${userChoice.toLowerCase()}, computer chose ${computerChoice.toLowerCase()},\nYou ${status}`);
+        
+        gameResult(`${gameData.userName} chose ${userChoice.toLowerCase()}, computer chose ${computerChoice.toLowerCase()},\nYou ${status}`);
     }
 
-    // Function that controls the game
-
-    function gameResult(result) {
-    
-        // Set Data 
-        roundDecision.textContent = result;
-        if (result.includes('Won'))
-        {
+    function getScore(result)
+    {
+        if (result.includes('Won')) {
             userNameScore.textContent = `${gameData.userName}: ${++gameData.userScore}`;
             userScoreDiv.appendChild(userNameScore);
+            userScoreDiv.style.backgroundColor = 'lightGreen';
+            setScoresWhite(false, true);
         }
-        else if (result.includes('Lost'))
-        {
+        else if (result.includes('Lost')) {
             computerNameScore.textContent = `Computer: ${++gameData.computerScore}`;
             computerScoreDiv.appendChild(computerNameScore);
+            computerScoreDiv.style.backgroundColor = 'red';
+            setScoresWhite(true);
         }
+        else {
+            setScoresWhite();
+        }
+    }
+
+    // Calculate game results
+    function gameResult(result) {
+    
+        roundDecision.textContent = result;
+        getScore(result);
 
         if (gameData.userScore === 5 || gameData.computerScore === 5)
         {
             buttonChoice.forEach(i => i.style.display = 'none');
             playButton.style.display = 'flex';
-            gameDecision.textContent = `Game end!! You ${gameData.userScore > gameData.computerScore ? 'Won' : 'Lost'}.\nPlay again?`
-            if (gameDecision.textContent.includes('Won'))
-            {
-                changeTitleColor();
-                intervalId = setInterval(changeTitleColor, 1000);
+            playButton.innerHTML = '<h1>Click here to play again</h1>';
+            gameDecision.textContent = `Game end!! You ${gameData.userScore > gameData.computerScore ? 'won' : 'lost'} the game. Play again?`;
 
+            if (gameDecision.textContent.includes('won')) {
+                changeTitleColor();
+                changeButtonColor();
+                changeScoreColor();
+                titleColorTimer = setInterval(changeTitleColor, 1000);
+                buttonColorTimer = setInterval(changeButtonColor, 1000);
+                scoreColorTimer = setInterval(changeScoreColor, 1000);
             }
             else {
-                allTitles.forEach(i => i.style.color = 'grey');
+                playButton.style.backgroundColor = "grey";
+                playButton.style.color = "black";
             }
         } 
     }
